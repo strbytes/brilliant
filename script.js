9008cc3c836a11a091338d09e93f794ff3e7350d
strbytes <strbytes@gmail.com>
2022-04-26 17:16:33 -0400

    Program now uses correct formula and rounds result

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const repInput = document.querySelector("#reps");
 const rpeInput = document.querySelector("#rpe");
 const repSelector = document.querySelector("#rep-selector");
 const submitButton = document.querySelector("#submit");
-const outputNode = document.querySelector("#main-output");
+const e1rmOutput = document.querySelector("#e1rm");
+const resultsOutput = document.querySelector("#results");
 
 submitButton.addEventListener("click", calculateBrilliant);
 
@@ -23,12 +24,30 @@ function estimatedMax(weight, reps, rpe) {
   return weight / (1.0278 - 0.0278 * (reps + (10 - rpe)));
 }
 
+function brilliantMax(weight, reps, rpe) {
+  // uses modified Brzycki formula
+  return weight / (1.0278 - 0.0278 * (reps + (9 - rpe)));
+}
+
 function calculateBrilliant(_) {
-  weight = +weightInput.value;
-  reps = +repInput.value;
-  rpe = +rpeInput.value;
-  e1rm = estimatedMax(weight, reps, rpe);
+  let weight = +weightInput.value;
+  let reps = +repInput.value;
+  let rpe = +rpeInput.value;
+  let e1rm = estimatedMax(weight, reps, rpe);
+  let brilliant1RM = brilliantMax(weight, reps, rpe);
   let selectedReps = repSelector.value;
-  let outputWeight = Math.round(e1rm * brilliantPercents[selectedReps]);
-  outputNode.textContent = `${outputWeight} recommended for sets of ${selectedReps}`;
+  let outputWeight = brilliant1RM * brilliantPercents[selectedReps];
+  displayResults(round2point5(e1rm), round2point5(outputWeight));
+}
+
+function displayResults(e1rm, outputWeight) {
+  e1rmOutput.textContent = `Estimated 1RM: ${e1rm}`;
+  resultsOutput.textContent = `Suggested weight: ${outputWeight}`;
+}
+
+function round2point5(n) {
+  tens = Math.floor(n / 10) * 10;
+  remainder = (n - tens) / 10;
+  round_remainder = Math.round(remainder * 4) / 4;
+  return tens + round_remainder * 10;
 }
